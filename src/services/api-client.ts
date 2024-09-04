f61ef3a65a9922e2e59bf7ff379eb9552f7e2e62
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-09-04 15:36:05 +0100

    Initial Commit

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,24 +9,6 @@ export interface FetchResponse<T> {
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "9cb27d142dba4ad4a1caa5a44195f0b2",
+    key: process.env.GAME_HUB_API_KEY,
   },
 });
-
-// class ApiClient<T> {
-//   endpoint: string;
-//   constructor(endpoint: string) {
-//     this.endpoint = endpoint;
-//   }
-
-//   getAll = (config: AxiosRequestConfig) => {
-//     return axiosInstance
-//       .get<FetchResponse<T>>(this.endpoint, config)
-//       .then((res) => res.data);
-//   };
-
-//   get = (id: string | number) =>
-//     axiosInstance.get<T>(this.endpoint + "/" + id).then((res) => res.data);
-// }
-
-// export default ApiClient;
